f0f3ce2c6d923a87948c10194c7e7fbdc34140c6
Kamil <bernacikkamil@gmail.com>
2024-11-29 23:45:08 +0100

    Add login and regiser peages

--- shopping-list/src/app/app.component.ts
+++ shopping-list/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {}
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('user');
+  }
+
+  onLogout(): void {
+    localStorage.removeItem('user');
+    this.router.navigate(['/auth/login']);
+  }
+}
