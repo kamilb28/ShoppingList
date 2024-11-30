a4a5c5605552ce0500a4bf92429730011e0701db
Kamil Bernacik <kbernacik@core-logic.com>
2024-11-30 15:30:25 +0100

    Fixes with logging and authorization

--- shopping-list/src/app/auth/login/login.component.ts
+++ shopping-list/src/app/auth/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  imports: [RouterModule],
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
@@ -15,7 +17,7 @@ export class LoginComponent {
     this.authService.login(username, password).subscribe({
       next: (response) => {
         localStorage.setItem('access_token', response.access_token);
-        this.router.navigate(['/shopping-list']);
+        this.router.navigate(['/list']);
       },
       error: (err) => {
         alert('Invalid username or password');
